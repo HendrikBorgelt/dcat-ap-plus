42ff1c54703d97fd84d324b9e849389c16d605d1
HendrikBorgelt <84382772+HendrikBorgelt@users.noreply.github.com>
2026-02-27 14:34:41 +0100

    changes in the pacakges to account for correct shacl generation

--- src/dcat_ap_plus/datamodel/dcat_ap_plus.py
+++ src/dcat_ap_plus/datamodel/dcat_ap_plus.py
@@ -1,5 +1,5 @@
 # Auto generated from dcat_ap_plus.yaml by pythongen.py version: 0.0.1
-# Generation date: 2026-01-26T14:14:50
+# Generation date: 2026-01-28T12:51:06
 # Schema: dcat-ap-plus
 #
 # id: https://w3id.org/nfdi-de/dcat-ap-plus/
